2ff12ed0d31a7d8b8114b026dd2e59f70c74401a
pintuprime <pintu.prime@25gmail.com>
2024-01-23 20:47:24 +0000

    Initial commit

---
+++ src/main/java/com/restful/booker/model/AuthPojo.java
@@ -0,0 +1,23 @@
+package com.restful.booker.model;
+
+public class AuthPojo {
+
+    public String username;
+    public String password;
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String userName) {
+        this.username = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
